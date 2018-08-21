8baaf344adb5bc8bb89144044819ce8b1f5f748f
Damian Paul <damianpaul.93@gmail.com>
2018-08-21 17:41:07 +0200

    Notification message sample

--- src/main/java/com/app/memoryquiz/FXMLController.java
+++ src/main/java/com/app/memoryquiz/FXMLController.java
@@ -1,5 +1,7 @@
 package com.app.memoryquiz;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,11 +11,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 import model.Lexicon;
 import model.LexiconLogic;
+import org.controlsfx.control.Notifications;
 
 public class FXMLController implements Initializable {
     @FXML
@@ -26,6 +33,8 @@ public class FXMLController implements Initializable {
     List<Lexicon> buttonLexicon;
     List<Button> buttonList = new ArrayList<Button>();
     int wordCounter = 4; //button spawn count
+    int questionCounter = 10; //how many spawns
+    int questionCounterInit = 0;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -53,11 +62,19 @@ public class FXMLController implements Initializable {
             button.setOnAction(new EventHandler<ActionEvent>(){
                 @Override public void handle (ActionEvent e){
                    if(x.id==labelLexicon.id){
-                        System.out.println("Correct");
-                        buttonList.clear();
-                        initializeButtonLogic();
+                       questionCounterInit++;
+                       System.out.println(questionCounterInit);
+                       System.out.println("Correct");
+                       buttonList.clear();
+                       initializeButtonLogic();
+                       result();
                    } else {
-                        System.out.println("Fail");
+                       questionCounterInit++;
+                       System.out.println(questionCounterInit);
+                       System.out.println("Fail");
+                       buttonList.clear();
+                       initializeButtonLogic();
+                       result();
                    }
                 }
             });
@@ -75,5 +92,26 @@ public class FXMLController implements Initializable {
             x.setText(buttonLexicon.get(n).translation);
             n++;
         }   
+    }
+    
+    private void result(){
+        Image img = new Image("/icons/ok_img1.png");
+        Notifications notificationBuilder = Notifications.create()
+                .title("Result")
+                .text("Mistakes:")
+                .graphic(new ImageView(img))
+                .hideAfter(Duration.seconds(5))
+                .position(Pos.TOP_RIGHT)
+                .onAction(new EventHandler<ActionEvent>(){
+                    @Override
+                    public void handle(ActionEvent event){
+                        System.out.println("Clicked on notification");
+                    }
+                });
+        notificationBuilder.darkStyle();
+        if(questionCounterInit==questionCounter){
+            notificationBuilder.show();
+            questionCounterInit=0;
+        }           
     }
 }
